1e8f7b34722a129f811ef4b34063028b7112f041
kenkwanatgai <kjjkjjzyayufqza@yahoo.com>
2023-06-08 16:21:38 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,3 @@ import HomePage from './Home/page';
 export default async function Home () {
   return <HomePage />;
 }
-
-export async function getServerSideProps() {
-  const data = JSON.stringify({ time: new Date() });
-  return { props: { data } };
-}
