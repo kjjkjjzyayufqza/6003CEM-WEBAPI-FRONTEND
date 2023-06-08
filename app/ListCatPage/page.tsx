e37c4ee102c440643bd01f9927361fcb98105209
kenkwanatgai <kjjkjjzyayufqza@yahoo.com>
2023-06-08 20:16:52 +0800

    test

--- app/ListCatPage/page.tsx
+++ app/ListCatPage/page.tsx
@@ -1,29 +1,55 @@
 'use client';
-import { Card } from 'antd';
+import { Card, Col, Row } from 'antd';
 import Meta from 'antd/es/card/Meta';
 import { useEffect } from 'react';
-
+import { HeartTwoTone } from '@ant-design/icons';
+import FilterBox from './FilterBox';
 export default function ListCatPage () {
-  useEffect(() => {
-    // console.log(session)
-  }, []);
+  useEffect(() => {}, []);
   return (
     <>
-      <div className='w-full max-w-xl px-5 xl:px-0'>
-        <div className='w-full max-w-xl px-5 xl:px-0'>
-          <Card
-            className='card bg-base-100 w-96 shadow-xl'
-            hoverable
-            cover={
-              <img
-                alt='example'
-                src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
-              />
+      <div className='w-full max-w-xl px-5 xl:px-0 '>
+        <FilterBox />
+      </div>
+      <div className='animate-fade-up my-10 grid w-full max-w-screen-xl grid-cols-1 gap-5 px-5 md:grid-cols-4 xl:px-0'>
+        <Card
+          className='card bg-base-100 overflow-hidden shadow-md'
+          hoverable
+          cover={
+            <img
+              alt='example'
+              src='https://cdn2.thecatapi.com/images/MTU3Njg1Mg.jpg'
+              className='transition delay-150 duration-300 ease-in-out hover:-translate-y-0 hover:scale-110 hover:bg-indigo-500'
+            />
+          }
+          actions={[<HeartTwoTone key='setting'/>]}
+        >
+          <Meta
+            title={
+              <div>
+                <Row>
+                  <Col span={8}>Name:</Col>
+                  <Col span={16}>jojo</Col>
+                </Row>
+              </div>
             }
-          >
-            <Meta title='Europe Street beat' description='www.instagram.com' />
-          </Card>
-        </div>
+            description={
+              <div>
+                <Row>
+                  <Col span={8}>Id:</Col>
+                  <Col span={16}>225669</Col>
+                  <Col span={8}>出生日期:</Col>
+                  <Col span={16}>19/11/2009</Col>
+                  <Col span={8}>中心:</Col>
+                  <Col span={16}>香港領養中心特殊需要</Col>
+                  <Col span={8}>品種:</Col>
+                  <Col span={16}>唐狗</Col>
+                </Row>
+              </div>
+            }
+            className=''
+          />
+        </Card>
       </div>
     </>
   );
--- components/home/card.tsx
+++ components/home/card.tsx
@@ -25,37 +25,39 @@ export const HomeCard: FC<HomeCardModel> = ({
         large ? 'md:col-span-2' : ''
       }`}
     >
-      <Link href={'/ListCatPage'}>
-        <div className='flex h-60 items-center justify-center'>{demo}</div>
-        <div className='mx-auto max-w-md text-center'>
-          <h2 className='font-display bg-gradient-to-br from-black to-stone-500 bg-clip-text text-xl font-bold text-transparent md:text-3xl md:font-normal'>
-            <Balancer>{title}</Balancer>
-          </h2>
-          <div className='prose-sm md:prose -mt-2 leading-normal text-gray-500'>
-            <Balancer>
-              <ReactMarkdown
-                components={{
-                  a: ({ node, ...props }) => (
-                    <a
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      {...props}
-                      className='font-medium text-gray-800 underline transition-colors'
-                    />
-                  ),
-                  code: ({ node, ...props }) => (
-                    <code
-                      {...props}
-                      // @ts-ignore (to fix "Received `true` for a non-boolean attribute `inline`." warning)
-                      inline='true'
-                      className='rounded-sm bg-gray-100 px-1 py-0.5 font-mono font-medium text-gray-800'
-                    />
-                  ),
-                }}
-              >
-                {description}
-              </ReactMarkdown>
-            </Balancer>
+      <Link href={'/ListCatPage'} passHref legacyBehavior>
+        <div>
+          <div className='flex h-60 items-center justify-center'>{demo}</div>
+          <div className='mx-auto max-w-md text-center'>
+            <h2 className='font-display bg-gradient-to-br from-black to-stone-500 bg-clip-text text-xl font-bold text-transparent md:text-3xl md:font-normal'>
+              <Balancer>{title}</Balancer>
+            </h2>
+            <div className='prose-sm md:prose -mt-2 leading-normal text-gray-500'>
+              <Balancer>
+                <ReactMarkdown
+                  components={{
+                    a: ({ node, ...props }) => (
+                      <a
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        {...props}
+                        className='font-medium text-gray-800 underline transition-colors'
+                      />
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code
+                        {...props}
+                        // @ts-ignore (to fix "Received `true` for a non-boolean attribute `inline`." warning)
+                        inline='true'
+                        className='rounded-sm bg-gray-100 px-1 py-0.5 font-mono font-medium text-gray-800'
+                      />
+                    ),
+                  }}
+                >
+                  {description}
+                </ReactMarkdown>
+              </Balancer>
+            </div>
           </div>
         </div>
       </Link>
